de1e3d34ea3e66346924f5e6086bc139ff1ebb53
Daniel Eidlin <daniel.eidlin@gmail.com>
2020-04-01 19:53:02 +0300

    Add channel_name field to Attacker and Victim models

--- reverse_shell/models.py
+++ reverse_shell/models.py
@@ -9,6 +9,7 @@ class Victim(models.Model):
     computer_name = models.CharField(max_length=200, blank=True)
     logged_in = models.BooleanField(default=True)
     owner = models.OneToOneField(User, related_name='victim', on_delete=models.CASCADE)
+    channel_name = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.computer_name} {self.ip} {self.port}'
@@ -21,6 +22,7 @@ class Attacker(models.Model):
     computer_name = models.CharField(max_length=200, blank=True)
     victim = models.OneToOneField(Victim, on_delete=models.SET_NULL, blank=True, null=True)
     owner = models.OneToOneField(User, related_name='attacker', on_delete=models.CASCADE)
+    channel_name = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.computer_name} {self.ip} {self.port}'
